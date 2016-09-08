e3a0eba2192e7c3ea6330f1a26f8f2c9d759cd5b
huangningfa <851103829@qq.com>
2016-09-08 13:53:47 +0800

    初始化项目

---
+++ BullsEye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BullsEye
+//
+//  Created by HNF's wife on 16/9/8.
+//  Copyright © 2016年 huangningfa@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BullsEye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BullsEye
+//
+//  Created by HNF's wife on 16/9/8.
+//  Copyright © 2016年 huangningfa@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
